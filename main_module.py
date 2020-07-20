86fcc5632737cb863299354c39d93946827e2cff
Vadim Timakin <vdm.timakin@yandex.ru>
2020-07-20 21:30:22 +0300

    sorter

--- main_module.py
+++ main_module.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import imghdr
 
 
 def folder_unpacker(current_root, new_root, target_type=None):
@@ -10,7 +11,8 @@ def folder_unpacker(current_root, new_root, target_type=None):
     Arguments:
         current_root - The source folder from which files are extracted,
         at the top of the hierarchy.
-        new_root - The folder to which the extracted files will be copied.
+        new_root - The folder to which the extracted files will be copied,
+        must be created in advance.
         target_type - The type of the target files
         (Example: jpg, pdf, png; without a dot at the beginning);
         if you want to extract files of the diffrent type at the same type,
@@ -20,6 +22,7 @@ def folder_unpacker(current_root, new_root, target_type=None):
     The function does not perform any conversions to the original folder,
     files are not deleted after copying to a new folder.
     """
+
     # Retrieving only certain types.
     if target_type:
 
@@ -51,3 +54,45 @@ def folder_unpacker(current_root, new_root, target_type=None):
             for file in files:
                 if os.path.isdir(root):
                     shutil.copy(os.path.join(root, file), new_root)
+
+
+def sorter(current_root, new_root):
+    """
+    Sorts files by their types.
+
+    This function extracts all files from the source folder and sorts them in
+    the target folder. The sorting process consists of creating several
+    new folders in the target folder with names of different file types
+    and then copying the corresponding files there.
+
+    The function ignores other folders with files inside the source folder.
+    If you also need to sort them, use the folder_unpacker function from
+    the same module to extract files from them in advance.
+
+    Arguments:
+        current_root - Source folder with unsorted files
+        new_root - The target folder where the sorted files will be located,
+        must be created in advance.
+
+    The function does not perform any conversions to the original folder,
+    files are not deleted after copying to a new folder.
+    """
+
+    for root, dirs, files in os.walk(current_root):
+        for file in files:
+            # This flag checks that our file is not a folder,
+            # which would give an error.
+            if os.path.isfile(os.path.join(root, file)):
+                file_type = imghdr.what(os.path.join(root, file))
+                # This flag checks that the file_type variable
+                # is not 'NoneType', which helps avoid TypeError.
+                # It sometimes happens that imghdr defines the type of a normal
+                # file (for example, a jpg image) as None.
+                if file_type:
+                    # Create a folder with a name containing the type of files
+                    # inside it, if this folder has not been created yet.
+                    if not os.path.exists(os.path.join(new_root, file_type)):
+                        os.mkdir(os.path.join(new_root, file_type))
+                    # Copying file.
+                    shutil.copy(os.path.join(root, file),
+                                os.path.join(new_root, file_type))
