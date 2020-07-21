9ab2de4865141910fdc11fac055423b1cee67902
Vadim Timakin <vdm.timakin@yandex.ru>
2020-07-21 11:07:22 +0300

    sorter refactoring

--- main_module.py
+++ main_module.py
@@ -72,7 +72,7 @@ def sorter(current_root, new_root, target_type=None):
 
     Args:
         current_root (str): Source folder with unsorted files
-        new_root (str): The target folder where the sorted files will be 
+        new_root (str): The target folder where the sorted files will be
         located, must be created in advance.
         target_type (:obj: ('str', 'tuple', list'), optional):
         Defaults to None. The type of the target files
@@ -85,80 +85,40 @@ def sorter(current_root, new_root, target_type=None):
     files are not deleted after copying to a new folder.
     """
 
-    # The code for this function could be made much smaller if I just set the
-    # check flag for the target_type variable inside the main loop. In this
-    # case, the flag would have to be checked every time the cycle passes,
-    # which would affect the performance, which is especially noticeable when
-    # working with large datasets.
-
     # Sorting only certain types.
     if target_type:
 
         # Working with multiple file types.
         if len(target_type) > 1:
-            for root, dirs, files in os.walk(current_root):
-                for file in files:
-                    # This flag checks that our file is not a folder,
-                    # which would give an error.
-                    if os.path.isfile(os.path.join(root, file)):
-                        file_type = imghdr.what(os.path.join(root, file))
-                        # This flag checks that the file_type variable is not
-                        # 'NoneType', which helps avoid TypeError. It
-                        # sometimes happens that imghdr defines the type of a
-                        # normal file (for example, a jpg image) as None.
-                        if file_type and (file_type in target_type):
-                            # Create a folder with a name containing the type
-                            # of files inside it, if this folder has not been
-                            # created yet.
-                            if not os.path.exists(
-                                    os.path.join(new_root, file_type)):
-                                os.mkdir(os.path.join(new_root, file_type))
-                            # Copying file.
-                            shutil.copy(os.path.join(root, file),
-                                        os.path.join(new_root, file_type))
+            def flag(x, y): return x in y
 
         # Working with a single file type.
         else:
-            for root, dirs, files in os.walk(current_root):
-                for file in files:
-                    # This flag checks that our file is not a folder,
-                    # which would give an error.
-                    if os.path.isfile(os.path.join(root, file)):
-                        file_type = imghdr.what(os.path.join(root, file))
-                        # This flag checks that the file_type variable is not
-                        # 'NoneType', which helps avoid TypeError. It
-                        # sometimes happens that imghdr defines the type of a
-                        # normal file (for example, a jpg image) as None.
-                        if file_type and (file_type == target_type):
-                            # Create a folder with a name containing the type
-                            # of files inside it, if this folder has not been
-                            # created yet.
-                            if not os.path.exists(
-                                    os.path.join(new_root, file_type)):
-                                os.mkdir(os.path.join(new_root, file_type))
-                            # Copying file.
-                            shutil.copy(os.path.join(root, file),
-                                        os.path.join(new_root, file_type))
+            def flag(x, y): return x == y
 
     # Sorting all files that are not folders.
     else:
-        for root, dirs, files in os.walk(current_root):
-            for file in files:
-                # This flag checks that our file is not a folder,
-                # which would give an error.
-                if os.path.isfile(os.path.join(root, file)):
-                    file_type = imghdr.what(os.path.join(root, file))
-                    # This flag checks that the file_type variable is not
-                    # 'NoneType', which helps avoid TypeError. It sometimes
-                    # happens that imghdr defines the type of a normal file
-                    # (for example, a jpg image) as None.
-                    if file_type:
-                        # Create a folder with a name containing the type of
-                        # files inside it, if this folder has not been
-                        # created yet.
-                        if not os.path.exists(
-                                os.path.join(new_root, file_type)):
-                            os.mkdir(os.path.join(new_root, file_type))
-                        # Copying file.
-                        shutil.copy(os.path.join(root, file),
-                                    os.path.join(new_root, file_type))
+        target_type = True
+        def flag(x, y): return bool(x or y)
+
+    for root, dirs, files in os.walk(current_root):
+        for file in files:
+            # This flag checks that our file is not a folder,
+            # which would give an error.
+            if os.path.isfile(os.path.join(root, file)):
+                file_type = imghdr.what(os.path.join(root, file))
+                # This flag checks that the file_type variable is not
+                # 'NoneType', which helps avoid TypeError. It sometimes
+                # happens that imghdr defines the type of a normal file
+                # (for example, a jpg image) as None.
+                if file_type and flag(file_type, target_type):
+                    # Create a folder with a name containing the type of
+                    # files inside it, if this folder has not been
+                    # created yet.
+                    if not os.path.exists(
+                            os.path.join(new_root, file_type)):
+                        os.mkdir(os.path.join(new_root, file_type))
+                    # Copying file.
+                    shutil.copy(os.path.join(root, file),
+                                os.path.join(new_root, file_type))
+                    
